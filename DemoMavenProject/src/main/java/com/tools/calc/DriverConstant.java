8dc9cc28435ad59b0022b063015cc65ac4f5b18c
Stuart Walker <stuart.walker@hcl.com>
2023-05-30 18:21:05 +0100

    remove old assets

--- DemoMavenProject/src/main/java/com/tools/calc/DriverConstant.java
+++
@@ -1,9 +0,0 @@
-package com.tools.calc;
-
-public class DriverConstant {
-
-	public static final String WIN_DRIVER = "D:\\onetest\\geckodriver.exe";
-	
-	public static final String LINUX_DRIVER = "/tmp/linux_x64_geckodriver";
-
-}
